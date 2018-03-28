c0474fdda5761eb353676ec44ed451633638780b
tjebbe.lievens@madeit.be <info@madeit.be>
2018-03-28 12:39:28 +0000

    List DNS records

---
+++ src/Object/DateTime.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MadeITBelgium\VestaCP\Object;
+
+/**
+ * VestaCP API.
+ *
+ * @version    0.0.1
+ *
+ * @copyright  Copyright (c) 2019 Made I.T. (http://www.madeit.be)
+ * @author     Made I.T. <info@madeit.be>
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-3.txt    LGPL
+ */
+trait DateTime
+{
+    private $time;
+    private $date;
+
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+}
